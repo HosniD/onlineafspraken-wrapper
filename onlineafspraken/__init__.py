c4aa2584efde28b4e0d42e842692da42f232b859
Jelmer Draaijer <info@jelmert.nl>
2021-07-15 11:26:35 +0200

    Bump version: 0.1.8 → 0.2.0

--- onlineafspraken/__init__.py
+++ onlineafspraken/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Hosni Dahmani"""
 __email__ = "hosni@hellotanit.com"
-__version__ = "0.1.8"
+__version__ = "0.2.0"
--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/HosniD/onlineafspraken",
-    version="0.1.8",
+    version="0.2.0",
     zip_safe=False,
 )
