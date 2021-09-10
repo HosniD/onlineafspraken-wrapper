710a9140570f3ca355906f73c00d70055f8b36ee
Jelmer Draaijer <info@jelmert.nl>
2021-09-10 09:29:55 +0200

    Bump version: 0.2.3 → 0.2.4

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/HosniD/onlineafspraken",
-    version="0.2.3",
+    version="0.2.4",
     zip_safe=False,
 )
